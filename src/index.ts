edfea2606b124738c550e75627ac670f439771e7
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-08-24 21:16:08 +0430

    rename CheckPersianNumber to checkPersianPhone

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export const ChangePersianToEnglish =(number:number) :number | undefined =>{
 }
 
 //This function get a persian @number and check it if valid or not and return boolean
-export const CheckPersianNumber =(number:string): boolean | undefined =>{
+export const CheckPersianPhone =(number:string): boolean | undefined =>{
     if(!number) return;
     const numberRegex = /^(\+98|0098|98|0)?9\d{9}$/;
     const res = numberRegex.test(number);
