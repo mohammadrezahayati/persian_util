9c375866df391aff1cb35eb1f0c6fce9965a00f6
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-08-24 14:21:22 +0430

    add eslint and prettier add ts watch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function multiply(a:number = 2, b:number = 4) {
-    return a * b;
+// made by @mohammadrezahayati
+
+
+//This Function get a persian @number and change it to english @number and return it.
+export const ChangePersianToEnglish =(number:number) :number | undefined =>{
+    if(!number) return;
+    return 12;
 }
+
+//This function get a persian @number and check it if valid or not and return boolean
+export const CheckPersianNumber =(number:number): boolean | undefined =>{
+    if(!number) return;
+    return true;
+}
+
+
+
+
+
