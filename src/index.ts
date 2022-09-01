9b5e899fd2f5420d31c98064eca3dbdfe12c10ae
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-09-01 16:10:59 +0430

    delete whatsapp platform

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * as parse from "./parser";
 export * as validation from "./validation";
 export * as constants from "./constants";
 export * as types from "./types";
-export * from "./webService";
