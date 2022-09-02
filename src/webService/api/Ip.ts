1214657bd24b86fff386ff3c9174dcafa47737a1
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-09-02 12:06:38 +0430

    some changes

--- src/webService/api/Ip.ts
+++ src/webService/api/Ip.ts
@@ -4,7 +4,7 @@ class IpApi {
     IpCreate = async (): Promise<AxiosInstance> => await axios.create({
         baseURL: `http://ip-api.com/json/`,
         timeout: 10000,
-    });
+    })
 }
 
 export default IpApi;
