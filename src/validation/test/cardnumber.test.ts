fbdff0c353b95df9398c3d0e2b1dda75748cb2a6
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-11-12 08:09:46 +0330

    cardNumber change

--- src/validation/test/cardnumber.test.ts
+++ src/validation/test/cardnumber.test.ts
@@ -4,7 +4,7 @@ describe("Card", () => {
     test("Card is Valid", () => {
         expect(CheckCardNumber("6037 9918 3333 4444")).toBeTruthy()
         expect(CheckCardNumber("5859831133334444")).toBeTruthy()
-        expect(CheckCardNumber("5022-2910-3333-4444")).toBeTruthy()
+        expect(CheckCardNumber("5022-2910-3333-4444")).toBeTruthy();
     })
     test("Bank Card name", () => {
         expect(CardBank("5022293345678976")).toStrictEqual("پاسارگاد");
