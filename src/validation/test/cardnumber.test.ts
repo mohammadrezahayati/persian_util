385725f7c3323b043197ab77ac7b14a7d9574cf9
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2022-11-14 19:00:20 +0330

    some changes

--- src/validation/test/cardnumber.test.ts
+++ src/validation/test/cardnumber.test.ts
@@ -2,7 +2,7 @@ import { describe, test, expect } from "@jest/globals"
 import { CheckCardNumber, CardBank } from "../"
 describe("Card", () => {
     test("Card is Valid", () => {
-        expect(CheckCardNumber("6037 9918 3333 4444")).toBeTruthy()
+        expect(CheckCardNumber("6037 9918 3333 4444")).toBeTruthy();
         expect(CheckCardNumber("5859831133334444")).toBeTruthy();
         expect(CheckCardNumber("5022-2910-3333-4444")).toBeTruthy();
     })
