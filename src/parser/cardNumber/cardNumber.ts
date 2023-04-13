a78f87fab3d602cfb0626d3a02c0c5ceb052e194
MRH <mohammadreza.hayati1375@gmail.com>
2023-04-13 13:04:16 +0430

    card number change

--- src/parser/cardNumber/cardNumber.ts
+++ src/parser/cardNumber/cardNumber.ts
@@ -1,3 +1,3 @@
 import { Fa_To_En } from "@Parser";
 
-export const CardNumberParse = (cardNumber: string): string => Fa_To_En(cardNumber).replace(/[\s.-]/g, "")
+export const CardNumberParse = (cardNumber: string): string => Fa_To_En(cardNumber).replace(/[\s.-]/g, "");
