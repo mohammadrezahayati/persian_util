a3585c71f5ca0e576ad69f91151bd15859ff5735
MRH <mohammadreza.hayati1375@gmail.com>
2023-04-19 18:34:48 +0330

    error class file added

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * as parse from "./parser";
 export * as validation from "./validation";
 export * as constants from "./constants";
 export * as types from "./types";
+export * as Error from "./error";
+export * as webServices from "./webService";
