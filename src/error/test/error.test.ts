63bcae5a66be0cd313239c6f620e9d627c35e88b
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2023-04-23 10:43:44 +0330

    Error class semi final test

--- src/error/test/error.test.ts
+++ src/error/test/error.test.ts
@@ -4,9 +4,9 @@ import { CardBank } from "../../validation/cardNumber";
 
 describe("Error", () => {
     test("Get Error response", () => {
-        const error = new Err(CardBank("5022291045674567"), true, EvalError() , "some asdasdasdasdasd");
+        const error = new Err(CardBank("5022291045674567"), "پاسارگاد", EvalError() , "some asdasdasdasdasd");
         expect(error.getError()).toBe(true);
-        const error1 = new Err(CardBank("502229104566678"), true, ReferenceError(), "some message");
-        expect(error1.getError()).toBe(false);
+        const error1 = new Err(CardBank("502229104566678"), false, ReferenceError(), "some message");
+        expect(error1.getError()).toBe(true);
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * as validation from "./validation";
 export * as constants from "./constants";
 export * as types from "./types";
 export * as Error from "./error";
+export * from "./error/error";
 export * as webServices from "./webService";
