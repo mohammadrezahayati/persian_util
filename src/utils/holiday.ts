cbc4e549b59f170ba460231b5add64177c485f66
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2023-04-26 18:35:43 +0330

    holiday skeleton added

---
+++ src/utils/holiday.ts
@@ -0,0 +1,12 @@
+import Holidays from "date-holidays";
+ 
+/**
+ * 
+ */
+export class Holiday extends Holidays {
+    constructor(private country: string){
+        super();
+        this.country = country;
+    }
+    
+}
