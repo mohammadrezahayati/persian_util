c3de54717ac19bcd8b78872a8ff349ad5495e736
MRH <mohammadreza.hayati1375@gmail.com>
2023-05-15 19:26:59 +0330

    some change in number

--- src/parser/number/index.ts
+++ src/parser/number/index.ts
@@ -1,6 +1,6 @@
 export * from "./FaToEn";
 export * from "./EnToFa";
-export * from "./addCommas"
+export * from "./addCommas";
 export * from "./removeCommas"
 export * from "./rialToToman"
 export * from "./tomanToRial"
