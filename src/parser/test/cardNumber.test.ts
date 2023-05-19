7f3a92b2ab59db73daf007e73202150ef0b819c5
MRH <mohammadreza.hayati1375@gmail.com>
2023-05-20 00:14:10 +0330

    some change in cardNumber.test.ts

--- src/parser/test/cardNumber.test.ts
+++ src/parser/test/cardNumber.test.ts
@@ -3,7 +3,7 @@ import { CardNumberParse } from "../cardNumber/cardNumber"
 describe("Card", () => {
     test("Pase CardNumber", () => {
         expect(CardNumberParse("1111-2222-3333-4444")).toStrictEqual("1111222233334444");
-        expect(CardNumberParse("1111 2222 3333 4444")).toStrictEqual("1111222233334444")
+        expect(CardNumberParse("1111 2222 3333 4444")).toStrictEqual("1111222233334444");
         expect(CardNumberParse("۱۱۱۱.۲۲۲۲.۳۳۳۳.۴۴۴۴")).toStrictEqual("1111222233334444")
     })
 })
