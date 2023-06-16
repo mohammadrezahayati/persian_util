495bde6a728470a7c982009f083f1d2006a61b4f
MRH <mohammadreza.hayati1375@gmail.com>
2023-06-16 10:46:22 +0330

    new version added

--- src/utils/test/holiday.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-import { Holiday } from '../holiday';
-describe('Card', () => {
-  test('Pase CardNumber', () => {
-    const hd = new Holiday('US');
-    expect(hd.getHolidays(2022)).toBe(false);
-  });
-});
