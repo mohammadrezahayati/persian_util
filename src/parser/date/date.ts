ce2bf5470d843d903acb3652f23077a47078ef14
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2023-08-07 00:20:28 +0330

    date duration base function added

--- src/parser/date/date.ts
+++ src/parser/date/date.ts
@@ -3,6 +3,13 @@ import { En_To_Fa } from './../number/EnToFa';
 //change time to some format
 //change format to real time
 
+/**
+ * The function calculates the difference in days between two given dates.
+ * @param {Date} a - The parameter "a" is a Date object representing the first date.
+ * @param {Date} b - The parameter "b" represents the second date for which you want to calculate the
+ * difference in days.
+ * @returns the difference in days between two given dates.
+ */
 export function dateDiffInDays(a: Date, b: Date) {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     // Discard the time and time-zone information.
