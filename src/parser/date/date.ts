b9b4cf89ddcebdda17cfff7ba0ef123a4a15b73b
mohammadrezahayati <mohammadreza.hayati1375@gmail.com>
2023-08-08 06:45:28 +0330

    date duration base function added

--- src/parser/date/date.ts
+++ src/parser/date/date.ts
@@ -11,6 +11,11 @@ import { En_To_Fa } from './../number/EnToFa';
  * @returns the difference in days between two given dates.
  */
 export function dateDiffInDays(a: Date, b: Date) {
+    /* The line `const _MS_PER_DAY = 1000 * 60 * 60 * 24;` is calculating the number of milliseconds in
+    a day. It multiplies the number of milliseconds in a second (1000) by the number of seconds in a
+    minute (60), the number of minutes in an hour (60), and the number of hours in a day (24). This
+    gives the total number of milliseconds in a day, which is then stored in the constant variable
+    `_MS_PER_DAY`. */
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     // Discard the time and time-zone information.
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
