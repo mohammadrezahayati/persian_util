90ed5a26f45489b3c9d18a768de4630679e85784
MRH <mohammadreza.hayati1375@gmail.com>
2023-08-17 10:39:32 +0330

    remove comments in dateDiffInDays function

--- src/parser/date/date.ts
+++ src/parser/date/date.ts
@@ -2,7 +2,6 @@ import { En_To_Fa } from './../number/EnToFa';
 //duration
 //change time to some format
 //change format to real time
-// some comments
 
 /**
  * The function calculates the difference in days between two given dates.
@@ -11,17 +10,12 @@ import { En_To_Fa } from './../number/EnToFa';
  * difference in days.
  * @returns the difference in days between two given dates.
  */
-export function dateDiffInDays(a: Date, b: Date) {
-
+export const dateDiffInDays = (a: Date, b: Date) => {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
     const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-
     return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
-
 
 const a = new Date("2017-01-01"),
     b = new Date("2017-06-25"),
