1a438866f368094023837100bb26d236722c9a4a
MRH <mohammadreza.hayati1375@gmail.com>
2023-08-20 06:44:44 +0330

    Iban function change

--- src/validation/cardNumber/Iban.ts
+++ src/validation/cardNumber/Iban.ts
@@ -7,9 +7,9 @@
  * @returns a boolean value. It returns `true` if the remainder of `concat % BigInt(97)` is equal to
  * `BigInt(10)`, and `false` otherwise.
  */
-export const IBANChecker = (sheba: string) => {
-    const firstChar = sheba.replace("IR", "1827").slice(0, 7);
-    const concat = BigInt(sheba.slice(5,).concat(firstChar)) 
-    console.log(concat % BigInt(97))
-    return concat % BigInt(97) === BigInt(10) ? true : false
-}
+export const IBANChecker = (sheba: string) => BigInt(sheba.slice(5,).concat(sheba.replace("IR", "1827").slice(0, 7))) % BigInt(97) === BigInt(10) ? true : false
+
+
+console.log(IBANChecker("IR550700001000119603807001"))
+console.log(IBANChecker("IR270600300870014508972001"))
+console.log(IBANChecker("IR560170000000345887268001"))
