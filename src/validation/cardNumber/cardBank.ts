356547216f1cf9fed1b570583ad10d7b3b5a57d5
MRH <mohammadreza.hayati1375@gmail.com>
2023-09-08 09:43:15 +0330

    validation/cardNumber/cardBank.ts  added comment

--- src/validation/cardNumber/cardBank.ts
+++ src/validation/cardNumber/cardBank.ts
@@ -1,5 +1,16 @@
+/* The line `import { Fa_To_En } from './../../parser/number/FaToEn';` is importing the `Fa_To_En`
+function from the `FaToEn` module located in the `parser/number` directory. This function is likely
+used to convert Persian (Farsi) numbers to English numbers. */
 import { Fa_To_En } from './../../parser/number/FaToEn';
+
+/* The line `import { banks } from '../../constants/cardNumber';` is importing the `banks` array from
+the `cardNumber` module located in the `constants` directory. This array likely contains information
+about different banks and their associated credit card number prefixes. */
 import { banks } from '../../constants/cardNumber';
+
+/* The line `import { CheckCardNumber } from './checkCardNumber';` is importing the `CheckCardNumber`
+function from the `checkCardNumber` module. This function is likely used to validate whether a given
+credit card number is valid or not. */
 import { CheckCardNumber } from './checkCardNumber';
 
 /**
